e905d493a73cdc3d3488396b615ee0cd76acafb1
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2024-08-29 20:29:37 +0300

    Support arrays

--- src/sql.ts
+++ src/sql.ts
@@ -1,10 +1,40 @@
-
-export type SqlQuery = { text: string, values: unknown[] }
+export type SqlQuery = { text: string; values: unknown[] }
 
 export const sql = (strings: TemplateStringsArray, ...values: unknown[]) => {
-  const text = strings.reduce((acc, str, i) => {
-    return `${acc}$${i + 1}${str}`
-  })
+  const initialValue: { text: string; currentVariableIndex: number } = {
+    text: '',
+    currentVariableIndex: 1,
+  }
+  const { text } = strings.reduce(
+    (acc, str, i) => {
+      const currentValue = values[i]
+      if (Array.isArray(currentValue)) {
+        const arrayVars = currentValue
+          .map((_, i) => `$${acc.currentVariableIndex + i}`)
+          .join(", ")
 
-  return { text, values }
+        return {
+          currentVariableIndex: acc.currentVariableIndex + currentValue.length,
+          text: `${acc.text}${str}${arrayVars}`,
+        }
+      } else if (i < strings.length - 1) {
+        return {
+          currentVariableIndex: acc.currentVariableIndex + 1,
+          text: `${acc.text}${str}$${acc.currentVariableIndex}`,
+        }
+      } else {
+        return {
+          currentVariableIndex: acc.currentVariableIndex,
+          text: `${acc.text}${str}`,
+        }
+      }
+    },
+    initialValue
+  )
+
+  const expandedValues = values.flatMap((value) =>
+    Array.isArray(value) ? value : [value],
+  )
+
+  return { text, values: expandedValues }
 }
--- test/sql.test.ts
+++ test/sql.test.ts
@@ -4,6 +4,22 @@ import { sql } from "../src/sql"
 
 describe('sql', () => {
   it('returns a SQL string', () => {
-    assert.equal(sql`SELECT * FROM users`.text, 'SELECT * FROM users');
+    assert.equal(sql`SELECT * FROM users`.text, 'SELECT * FROM users')
+  })
+
+  it('returns values', () => {
+    assert.deepEqual(sql`SELECT * FROM users WHERE id = ${1}`.values, [1])
+  })
+
+  it('supports array values', () => {
+    const result = sql`SELECT * FROM users WHERE id = ANY(${[1, 2, 3]})`
+    assert.equal(result.text, 'SELECT * FROM users WHERE id = ANY($1, $2, $3)')
+    assert.deepEqual(result.values, [1, 2, 3])
+  })
+
+  it('handles array values correctly when there are multiple placeholders', () => {
+    const result = sql`SELECT * FROM users WHERE id = ANY(${[1, 2, 3]}) AND name = ${'Alice'}`
+    assert.equal(result.text, 'SELECT * FROM users WHERE id = ANY($1, $2, $3) AND name = $4')
+    assert.deepEqual(result.values, [1, 2, 3, 'Alice'])
   })
 })
