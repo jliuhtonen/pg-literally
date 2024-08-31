d9f908419060866182b5bb5efc398f0a13d04a34
Janne Liuhtonen <janne.liuhtonen@iki.fi>
2024-08-31 20:48:46 +0300

    Omit extra traversal of values

--- src/sql.ts
+++ src/sql.ts
@@ -5,6 +5,7 @@ export type SqlQuery = { text: string; values: unknown[] }
 interface AccumulatedQueryState {
   text: string
   currentVariableIndex: number
+  values: unknown[]
 }
 
 const appendCurrentVariableToQueryFn =
@@ -16,21 +17,26 @@ const appendCurrentVariableToQueryFn =
   ): AccumulatedQueryState => {
     const currentValue = queryPartValues[currentQueryPartIndex]
     if (isSqlFragment(currentValue)) {
-      const { text: fragmentQueryText, currentVariableIndex } =
-        currentValue.strings.reduce(
-          appendCurrentVariableToQueryFn(
-            currentValue.strings.length,
-            currentValue.values,
-          ),
-          {
-            text: "",
-            currentVariableIndex: acc.currentVariableIndex,
-          },
-        )
+      const {
+        text: fragmentQueryText,
+        currentVariableIndex,
+        values: fragmentValues,
+      } = currentValue.strings.reduce(
+        appendCurrentVariableToQueryFn(
+          currentValue.strings.length,
+          currentValue.values,
+        ),
+        {
+          text: "",
+          currentVariableIndex: acc.currentVariableIndex,
+          values: [],
+        },
+      )
 
       return {
         currentVariableIndex,
         text: `${acc.text}${currentQueryPart}${fragmentQueryText}`,
+        values: [...acc.values, ...fragmentValues],
       }
     } else if (Array.isArray(currentValue)) {
       const arrayVars = currentValue
@@ -40,42 +46,38 @@ const appendCurrentVariableToQueryFn =
       return {
         currentVariableIndex: acc.currentVariableIndex + currentValue.length,
         text: `${acc.text}${currentQueryPart}${arrayVars}`,
+        values: [...acc.values, ...currentValue],
       }
     } else if (currentQueryPartIndex < numberOfQueryParts - 1) {
       return {
         currentVariableIndex: acc.currentVariableIndex + 1,
         text: `${acc.text}${currentQueryPart}$${acc.currentVariableIndex}`,
+        values: [...acc.values, currentValue],
       }
     } else {
       return {
         currentVariableIndex: acc.currentVariableIndex,
         text: `${acc.text}${currentQueryPart}`,
+        values: acc.values,
       }
     }
   }
 
-const expandValues = (values: unknown[]): unknown[] =>
-  values.flatMap((value) => {
-    if (isSqlFragment(value)) {
-      return expandValues(value.values)
-    } else if (Array.isArray(value)) {
-      return value
-    } else {
-      return [value]
-    }
-  })
-
 export const sql = (strings: TemplateStringsArray, ...values: unknown[]) => {
-  const initialValue: { text: string; currentVariableIndex: number } = {
+  const initialValue: {
+    text: string
+    currentVariableIndex: number
+    values: unknown[]
+  } = {
     text: "",
     currentVariableIndex: 1,
+    values: [],
   }
 
-  const { text } = strings.reduce(
+  const { text, values: expandedValues } = strings.reduce(
     appendCurrentVariableToQueryFn(strings.length, values),
     initialValue,
   )
-  const expandedValues = expandValues(values)
 
   return { text, values: expandedValues }
 }
